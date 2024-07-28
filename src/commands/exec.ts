ef295ea9850ef0d7c467e82e1f7473c3b05bfb6b
lafkpages <luisafk@proton.me>
2024-07-29 01:54:22 +0200

    feat: exec cmd --from and --to options, plus fix -n

--- src/commands/exec.ts
+++ src/commands/exec.ts
@@ -1,9 +1,13 @@
 import { Command } from "commander";
 import { log } from "..";
 import { join } from "node:path";
+import { validateBunVersion } from "../install";
+import { versionToTagName } from "../github";
 
 export default new Command("exec")
   .description("Execute several Bun versions with the given arguments")
+  .option("-f, --from <version>", "Lower bound of version range to execute")
+  .option("-t, --to <version>", "Upper bound of version range to execute")
   .option(
     "-n, --no-output",
     "Do not show stdout/stderr of Bun processes",
@@ -15,8 +19,20 @@ export default new Command("exec")
   )
   .action(async function (
     this: Command,
-    options: { installDir: string; noOutput: boolean }
+    options: {
+      from?: string;
+      to?: string;
+      installDir: string;
+      output: boolean;
+    }
   ) {
+    if (options.from) {
+      validateBunVersion(versionToTagName(options.from));
+    }
+    if (options.to) {
+      validateBunVersion(versionToTagName(options.to));
+    }
+
     const bunInstallations = (
       await Array.fromAsync(
         new Bun.Glob("bun-v?*/bin/bun").scan({
@@ -35,9 +51,18 @@ export default new Command("exec")
 
     for await (const [bunInstallation, version] of bunInstallations) {
       log.debug("Found Bun installation:", bunInstallation);
+
+      if (options.from && Bun.semver.order(version, options.from) === -1) {
+        continue;
+      }
+
+      if (options.to && Bun.semver.order(version, options.to) === 1) {
+        continue;
+      }
+
       log.info("Executing Bun version:", version);
 
-      const stdio = options.noOutput ? "ignore" : "inherit";
+      const stdio = options.output ? "inherit" : "ignore";
 
       const bunProcess = Bun.spawn({
         cmd: [join(options.installDir, bunInstallation), ...this.args],
