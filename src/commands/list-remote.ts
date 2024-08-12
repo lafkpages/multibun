645b85236e2682a7087fbe7446e1bc817f486c32
lafkpages <luisafk@proton.me>
2024-08-13 02:57:17 +0900

    feat!: list-remote limit shown versions, add --limit opt

--- src/commands/list-remote.ts
+++ src/commands/list-remote.ts
@@ -1,14 +1,32 @@
-import { Command } from "@commander-js/extra-typings";
+import { Command, Option, program } from "@commander-js/extra-typings";
 
 import { getAllReleases } from "../github";
 
 export default new Command("list-remote")
   .alias("lsr")
   .description("List available Bun versions")
-  .action(async () => {
+  .addOption(
+    new Option(
+      "-l, --limit <limit>",
+      "Limit the number of versions to list. -1 will list all versions.",
+    )
+      .default(25)
+      .argParser(parseInt),
+  )
+  .action(async (options) => {
+    if (options.limit === -1) {
+      options.limit = Infinity;
+    } else if (options.limit < 1) {
+      program.error("Limit must be -1 or a positive number.");
+    }
+
     const releases = await getAllReleases();
 
-    for (const release of releases) {
-      console.log("-", release.tagName);
+    for (let i = 0; i < Math.min(releases.length, options.limit); i++) {
+      console.log("-", releases[i].tagName);
+    }
+
+    if (options.limit < releases.length) {
+      console.log("...");
     }
   });
